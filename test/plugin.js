24f862df40a7f8dcf10ebf628b7d97150282a961
Mehdi Avdi <mehdi.avdi@gmail.com>
2014-04-23 13:11:07 +0100

    added test, checking for error using isBoom

---
+++ test/plugin.js
@@ -0,0 +1,36 @@
+// Load modules
+
+var Lab = require('lab');
+var Hapi = require('hapi');
+
+
+// Declare internals
+
+var internals = {};
+
+
+// Test shortcuts
+
+var expect = Lab.expect;
+var before = Lab.before;
+var after = Lab.after;
+var describe = Lab.experiment;
+var it = Lab.test;
+
+
+describe('bassmaster', function () {
+
+    it('can be added as a plugin to hapi', function (done) {
+
+        var server = new Hapi.Server();
+        server.pack.require('../', {
+          locales : ['en', 'es'],
+          defaultLocale : 'en',
+          directory : './test/locales'
+        }, function (err) {
+
+            expect(err).to.not.exist;
+            done();
+        });
+    });
+});
